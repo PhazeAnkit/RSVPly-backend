bc8dd9d69f68166fc9304ec86465d7bbeffb9db0
PhazeAnkit <104414296+PhazeAnkit@users.noreply.github.com>
2025-12-19 11:58:34 +0530

    event endpoints fix

--- apps/routes/event.routes.ts
+++ apps/routes/event.routes.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 import { eventController } from "../controllers/event.controller";
 import { auth } from "../middlewares/auth.middleware";
 const router = Router();
+router.get("/dashboard", auth, eventController.dashboard);
 router.post("/create", auth, eventController.create);
 router.get("/", auth, eventController.getAll);
 router.get("/:id", auth, eventController.getById);
 router.delete("/:id", auth, eventController.deleteEvent);
 router.post("/:id/registrations", auth, eventController.joinEvent);
 router.delete("/:id/registrations", auth, eventController.leaveEvent);
-router.get("/dashboard", auth, eventController.dashboard);
+
 
 export default router;

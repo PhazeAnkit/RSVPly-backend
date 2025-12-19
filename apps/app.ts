e782304e2d8daf2382b08caf3fd8b84c8456f5e3
Ankit Kumar Ojha <104414296+PhazeAnkit@users.noreply.github.com>
2025-12-19 10:13:33 +0530

    Update CORS configuration in app.ts

--- apps/app.ts
+++ apps/app.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 import eventRouter from "./routes/event.routes";
 
 const app = express();
-app.use(cors());
+app.use(cors({*}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
